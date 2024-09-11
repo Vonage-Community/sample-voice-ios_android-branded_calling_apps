5c2df0713da2ef2e14ab9ff37dddc9bdf38e6b44
Abdulhakim Ajetunmobi <abdul.ajetunmobi@vonage.com>
2024-09-11 15:53:24 +0100

    Update APIService.kt

--- Android/app/src/main/java/com/vonage/inapp_incoming_voice_call/api/APIService.kt
+++ Android/app/src/main/java/com/vonage/inapp_incoming_voice_call/api/APIService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface APIService {
-    @POST("user")
+    @POST("users")
     fun getCredential(
         @Body loginInformation: LoginInformation
     ): Call<User>
